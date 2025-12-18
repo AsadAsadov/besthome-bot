3080567d6106f328db6a6508b7dfb52a142ce6d7
AsadAsadov <esedbesthome@gmail.com>
2025-12-18 22:12:57 +0400

    Ensure safe UI cleanup handling

--- besthome_unified_bot.py
+++ besthome_unified_bot.py
@@ -198,7 +198,7 @@ def safe_clear_ui(bot, chat_id, message_ids):
     try:
         for mid in message_ids[-5:]:
             bot.delete_message(chat_id, mid)
-    except:
+    except Exception:
         pass
 
 
