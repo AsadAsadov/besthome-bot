a17d63a9e3e54e84a6d1cd6cd9b3ade86259c8f9
AsadAsadov <esedbesthome@gmail.com>
2026-05-16 17:57:21 +0400

    Delete static directory

--- static/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const chatArea = document.querySelector('.chat-area');
-  if (chatArea) {
-    chatArea.scrollTop = chatArea.scrollHeight;
-  }
-});
